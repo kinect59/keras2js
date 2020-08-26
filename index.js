c7abba79d630a01bb4effd2c24e6f39fa54388d4
kinect59 <hazem.wannous@univ-lille1.fr>
2020-08-26 23:20:10 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 async function loadModel(){ 
-              model = await tf.loadModel('XOR/web_model/model.json')
+              model = await tf.loadModel('https://github.com/kinect59/keras2js.github.io/XOR/web_model/model.json')
               y = model.predict(tf.zeros([1,2])) 
               document.getElementById('out').innerHTML = y.dataSync()[0] 
           } 
